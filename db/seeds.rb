b145089f69f2c6550d118452b44b026d17525868
kerry pang <kerrypang15@gmail.com>
2022-05-19 10:56:11 +0200

    rails yelp mvp incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+asian_kitchen = { name: "Asian Kitchen", address: "9 Bury St, London E2 456", category: "japanese" }
+indian_kitchen =  { name: "Indian Kitchen", address: "56A Shoreditch Low St, London E1 666", category: "italian" }
+malaysian_kitchen = { name: "Malaysian Kitchen", address: "7 Langstrasse St, London E2 888", category: "french" }
+
+[ dishoom, pizza_east, asian_kitchen, indian_kitchen, malaysian_kitchen ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
